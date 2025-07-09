a77278a6e7727a62f923ff7b3388b2bd3887006e
levham <eskibirhesap@gmail.com>
2025-07-09 17:57:58 +0300

    v.0.0.1
    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//App bileşeni render edildiğinde içinde "learn react" yazılı bir öğe var mı diye bakmak.
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />);//App bileşenini test ortamına render eder.
+  const linkElement = screen.getByText(/learn react/i);//DOM'da "learn react" ifadesini, büyük/küçük harf duyarsız şekilde arar.
+  expect(linkElement).toBeInTheDocument();//Öğenin sayfada gerçekten mevcut olup olmadığını doğrular.
 });
